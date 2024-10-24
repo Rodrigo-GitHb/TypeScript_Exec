39459b133ce6c6a18430f42e6ce1dbf453710e78
Rodrigo-GitHb <rodrigo291002@hotmail.com>
2024-10-24 19:08:13 -0300

    Commit

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(valor1, valor2) {
+    return console.log(valor1 * valor2);
+}
+function saudacoes(nome) {
+    return console.log(`Olá ${nome}`);
+}
+multiplica(20, 20);
+saudacoes("Rodrigo");
---
+++ funcoes.ts
@@ -0,0 +1,10 @@
+function multiplica(valor1: number, valor2: number){
+    return console.log(valor1 * valor2);
+}
+
+function saudacoes(nome: string){
+    return console.log(`Olá ${nome}`);
+}
+
+multiplica(20, 20)
+saudacoes("Rodrigo")
